33dd4881a788b8d916e79a5f6924327ebe38d454
Caio Colaiacovo Carneiro da Costa <caiocolaiacovo@gmail.com>
2026-05-17 11:18:31 -0400

    feat: updating collections file

--- dotnet/Dsa/Collections.cs
+++ dotnet/Dsa/Collections.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Specialized;
 using System.Diagnostics;
 
 namespace Dsa
@@ -8,43 +9,55 @@ namespace Dsa
     {
         public static void MainCollections()
         {
-            // List is a generic collection that stores elements in a dynamic list.
-            // It is based on an array and can automatically resize when needed.
-            // It is NOT THREAD-SAFE!
-            // BigO: O(1) for direct access at any position and insertion at the end, O(n) for insertion or removal at intermediate positions.
+            Console.WriteLine("Collections -----------------------");
+
+            /* 
+            List is a generic collection that stores elements in a dynamic list.
+            It is based on an array and can automatically resize when needed.
+            Implements the IList<T> interface, which allows for indexed access to elements and provides methods for adding, removing, and searching for elements.
+            Not thread-safe.
+            
+            Time complexity:
+                - O(1) for direct access at any position and insertion at the end
+                - O(n) for insertion or removal at intermediate positions
+            */
             var list = new List<string>();
             list.Add("Teste");
             list.Add("Teste2");
             list[1].ToString();
             Console.WriteLine(list[0]);
-            
-            var t = new int[]{};
 
-            // Basically, it represents a HashMap, but it is older and not generic.
-            // Note: Hashtable is considered a legacy collection and is generally not recommended for new development.
-            // Instead, it's better to use Dictionary<TKey, TValue> for most scenarios.
-            // It can store keys and values of any type, but this can lead to runtime type issues.
-            // Is is NOT thread-safe! It can be used "as thread-safe" using Hashtable.Synchronized, but this can lead to performance issues.
-            // BigO: O(1) for insertion, removal, and access operations, but O(n) in the worst case due to collisions.
+            /*
+            Basically, it represents a HashMap, but it is older and not generic. It can store keys and values of any type, but this can lead to runtime type issues. Not thread-safe.
+            Note: Hashtable is considered a legacy collection and is generally not recommended for new development. Instead, it's better to use Dictionary<TKey, TValue> for most scenarios.
+            
+            Time complexity: 
+            -   O(1) for insertion, removal, and access operations, but O(n) in the worst case due to collisions.
+            */
             var hashTable = new Hashtable();
             hashTable.Add(1, "Teste");
             hashTable.Add("foo", "Teste2");
             Console.WriteLine("Hashtable -------------");
             Console.WriteLine(hashTable["bar"]);
+            
+            /*
+            Dictionary is a generic collection that stores key-value pairs. It is considered a "modern" version of Hashtable, offering better performance and type safety. Not thread-safe.
 
-            // Dictionary is a generic collection that stores key-value pairs.
-            // It is considered a "modern" version of Hashtable, offering better performance and type safety.
-            // Is is NOT thread-safe!
-            // BigO: O(1) for insertion, removal, and access operations, but O(n) in the worst case due to collisions.
+            Time complexity: 
+                - O(1) for insertion, removal, and access operations, but O(n) in the worst case due to collisions.
+            */
             var dictionary = new Dictionary<string, string>();
             dictionary.Add("foo", "Teste2");
             // dictionary.Add("foo", "Teste3");
             Console.WriteLine("Dictionary -------------");
             Console.WriteLine(dictionary["foo"]);
 
-            // HashSet is a collection that stores unique elements, meaning it does not allow duplicates.
-            // Is is NOT thread-safe!
-            // BigO: O(1) for insertion, removal, and existence check operations, but O(n) in the worst case due to collisions.
+            /*
+            HashSet is a collection that stores unique elements, meaning it does not allow duplicates. Not thread-safe.
+            
+            Time complexity: 
+                - O(1) for insertion, removal, and existence check operations, but O(n) in the worst case due to collisions.
+            */
             var hashMap = new HashSet<string>();
             hashMap.Add("Teste");
             hashMap.Add("Teste2");
@@ -52,10 +65,13 @@ namespace Dsa
             Console.WriteLine("HashSet -------------");
             Console.WriteLine(hashMap.Contains("Teste3"));
 
-            // Stack is a LIFO (Last In, First Out) collection, meaning the last element added is the first one to be removed.
-            // NÃO é thread-safe!
-            // It is NOT thread-safe! It can be used "as thread-safe" using Stack.Synchronized, but this can lead to performance issues.
-            // BigO: O(1) for push and pop operations, O(n) for search.
+            /*
+            Stack is a LIFO (Last In, First Out) collection, meaning the last element added is the first one to be removed. Not thread-safe.
+
+            Time complexity: 
+                - O(1) for push and pop operations
+                - O(n) for search.
+            */
             var stack = new Stack<string>();
             stack.Push("Teste");
             stack.Push("Teste2");
@@ -65,11 +81,14 @@ namespace Dsa
             Console.WriteLine(stack.Pop());
             Console.WriteLine(stack.Pop());
 
-            // Queue é uma coleção que segue a ordem FIFO (First In, First Out), ou seja, o primeiro elemento adicionado é o primeiro a ser removido.
-            // Queue is a FIFO (First In, First Out) collection, meaning the first element added is the first one to be removed.
-            // NÃO é thread-safe!
-            // It is NOT thread-safe! It can be used "as thread-safe" using Queue.Synchronized, but this can lead to performance issues.
-            // BigO: O(1) for enqueue and dequeue operations, O(n) for search.
+            /*
+            Queue is a FIFO (First In, First Out) collection, meaning the first element added is the first one to be removed.
+            Not thread-safe.
+
+            Time complexity: 
+                - O(1) for enqueue and dequeue operations
+                - O(n) for search.
+            */
             var queue = new Queue<string>();
             queue.Enqueue("Teste");
             queue.Enqueue("Teste2");
@@ -78,13 +97,23 @@ namespace Dsa
             queue.Dequeue();
             Console.WriteLine(queue.Peek());
 
-            // ConcurrentDictionary
-            var text = "A Samsung apresentou nesta quarta (25) seus três novos celulares da linha Galaxy S26 com funcionalidades de IA avançadas e uma inédita função de tela com proteção de privacidade que não precisa de película.\nOs celulares Galaxy S26, S26+ e S26 Ultra foram anunciados em um evento em São Francisco, nos EUA. Eles já estão disponíveis em pré-venda e chegam às lojas em 20 de março. Confira os preços abaixo.\nOs três anunciados são vendidos com duas opções de armazenamento (256 GB e 512 GB) e 12 GB de RAM. O opções S26 Ultra tem ainda uma versão mais avançada, com 1 TB de armazenamento e 16 GB de RAM.\nSegundo a Samsung, os modelos são fabricados no Brasil. Com isso, eles não serão afetados pelo aumento do imposto de importação para celulares e outras centenas de produtos, anunciado no início de fevereiro pelo governo federal.\nA linha S26 concorre diretamente com o iPhone 17, lançado em setembro de 2025.O Galaxy S26 Ultra, modelo mais avançado da linha, é o que vem com a principal novidade: um modo de privacidade na tela contra curiosos.\nQuando ativada, apenas quem está na frente do aparelho consegue ver o conteúdo. Quem está ao lado vê apenas uma tela preta.\nA privacidade da tela, segundo a Samsung, é ativada de forma rápida no painel de controle do celular.Ela funciona desativando alguns pontos de luz(pixels) do display, o que também ajuda a economizar energia.";
-            WordCounterConcurrent(text);
-            // Dictionary + Lock
-            WordCounterLock(text);
+            /*
+            ConcurrentDictionary is a thread-safe version of the Dictionary for concurrent access.
+
+            Time complexity:
+                - O(1) for AddOrUpdate, GetOrAdd, TryGetValue, TryRemove
+            */
+            var text = System.IO.File.ReadAllText("TextCountNews.txt");
+            WordCounterWithConcurrentDictionary(text);
+            WordCounterDictionaryAndLock(text);
+
+            /*
+            BlockingCollection provides a thread-safe collection that can be used to produce and consume items in a producer-consumer scenario.
+            It wraps a concurrent collection with blocking Add/Take perations
             
-            // BlockingCollection -> Provides a thread-safe collection that can be used to produce and consume items in a producer-consumer scenario.
+            Time complexity:
+                - O(1) for Add and Take (when backed by ConcurrentQueue). 
+            */
             var blockingCollection = new BlockingCollection<string>();
             Console.WriteLine("BlockingCollection -------------");
             Console.WriteLine(blockingCollection.IsCompleted);
@@ -96,6 +125,7 @@ namespace Dsa
             blockingCollection.CompleteAdding();
             Console.WriteLine(blockingCollection.IsCompleted);
             Console.WriteLine(blockingCollection.IsAddingCompleted);
+            // Gets blocked until an item is available or the collection is marked as complete for adding and empty.
             var data = blockingCollection.Take();
             Console.WriteLine($"Data taken: {data}");
             Console.WriteLine(blockingCollection.IsCompleted);
@@ -106,22 +136,87 @@ namespace Dsa
             Console.WriteLine(blockingCollection.IsAddingCompleted);
             // data = blockingCollection.Take(); -> throws error
 
-            var priorityQueue = new PriorityQueue<string, int>();
-            priorityQueue.Enqueue("Teste", 200);
-            priorityQueue.Enqueue("Teste2", 10);
+            /*
+            PriorityQueue is a min/max-heap (depending on the comparer) based collection that returns elements by priority.
+            It uses an Array internally.
+            Not thread-safe.
+
+            Time complexity:
+                - O(log n) for Enqueue and Dequeue
+                - O(1) for Peek
+            */
+            var priorityQueue = new PriorityQueue<string, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+            priorityQueue.Enqueue("Teste", 10);
+            priorityQueue.Enqueue("Teste2", 200);
+            priorityQueue.Enqueue("Teste2", 150);
             priorityQueue.Enqueue("Teste3", 70);
             Console.WriteLine("PriorityQueue -------------");
             Console.WriteLine(priorityQueue.Dequeue());
             Console.WriteLine(priorityQueue.Dequeue());
             Console.WriteLine(priorityQueue.Dequeue());
 
+            /*
+            SortedDictionary maintains elements sorted by key only, using a binary tree (Red-Black tree).
+            Sort the keys only, not the values.
+            Not thread-safe.
+
+            Time complexity:
+                - O(log n) for Add, Remove, TryGetValue
+                - O(log n) for First (LINQ method, but SortedDictionary doesn't implement IList and doesn't have direct access to the first element, so it needs to traverse the collection)
+                - O(n) for Last (LINQ method, but SortedDictionary doesn't implement IList and doesn't have direct access to the first element, so it needs to traverse the collection)
+                - O(n) for enumeration
+            */
+            var sortedDictionary = new SortedDictionary<string, int>();
+            sortedDictionary.Add("banana", 8);
+            sortedDictionary.Add("apple", 10);
+            sortedDictionary.Add("pear", 1);
+            sortedDictionary.Add("orange", 2);
+            Console.WriteLine($"first: {sortedDictionary.First()}"); // [apple, 10]
+            Console.WriteLine($"last: {sortedDictionary.Last()}"); // [pear, 1]
+
+            /*
+            SortedSet maintains unique elements sorted by a specified comparer, using a binary tree (Red-Black tree). Not thread-safe.
+
+            Time complexity:
+                - O(log n) for Add, Remove, Contains
+                - O(n) for enumeration
+            */
+            var sortedSet = new SortedSet<File>(new FileComparer());
+            sortedSet.Add(new File("file1.txt", 100));
+            sortedSet.Add(new File("file2.txt", 50));
+            sortedSet.Add(new File("file3.txt", 150));
+            Console.WriteLine("SortedSet -------------");
+            foreach (var file in sortedSet)            {
+                Console.WriteLine($"{file.Path}: {file.Size}");
+            }
+
+            /*
+            OrderedDictionary maintains both an ArrayList and a HashTable, keeping insertion order and allowing access by index or by key.
+            Not thread-safe.
+
+            Time complexity:
+                - O(1) for access by index (on ArrayList)
+                - O(1) for access by key (on HashTable)
+                - O(1) for Insert
+                - O(n) for Remove (because the ArrayList must shift elements)
+            */
+            var orderedDictionary = new OrderedDictionary();
+            orderedDictionary.Add("banana", 8);
+            orderedDictionary.Add("apple", 10);
+            orderedDictionary.Add("pear", 1);
+            orderedDictionary.Add("orange", 2);
+            Console.WriteLine($"first: {orderedDictionary[0]}"); // [apple, 10]
+            Console.WriteLine($"last: {orderedDictionary[3]}"); // [pear, 1]
+
             // TODO:
             // ConcurrentQueue
             // ConcurrentStack
             // ConcurrentBag -> Thread-safe version of List<T>, but does not maintain the order of elements.
+
+            Console.WriteLine("End Collections ----------------------------------");
         }
 
-        public static void WordCounterConcurrent(string text)
+        public static void WordCounterWithConcurrentDictionary(string text)
         {
             var splitedText = text
                 .Replace(".", "")
@@ -154,7 +249,7 @@ namespace Dsa
             Console.WriteLine("ConcurrentDictionary Time: " + stopwatch.ElapsedMilliseconds + "ms");
         }
 
-        public static void WordCounterLock(string text)
+        public static void WordCounterDictionaryAndLock(string text)
         {
             var splitedText = text
                 .Replace(".", "")
@@ -188,6 +283,31 @@ namespace Dsa
                 Console.WriteLine($"Word: {kvp.Key}, Count: {kvp.Value}");
             }
             Console.WriteLine("Lock Time: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+
+    public class FileComparer : IComparer<File>
+    {
+        // Sort descending by size
+        public int Compare(File? x, File? y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return 1;
+            if (y == null) return -1;
+
+            return y.Size.CompareTo(x.Size);
+        }
+    }
+
+    public class File
+    {
+        public string Path { get; private set; }
+        public int Size { get; private set; }
+
+        public File(string path, int size)
+        {
+            Path = path;
+            Size = size;
         }
     }
 }
--- dotnet/Dsa/Program.cs
+++ dotnet/Dsa/Program.cs
@@ -5,20 +5,18 @@ public class Program
     public static void Main()
     {
         Collections.MainCollections();
-        // RangeSumQuery303.MainRangeSum();
-        // Threads.MainThreads();
-        // BinarySearch704.MainBinarySearch();
-        // BinaryTree.MainBinaryTree();
-        // InvertBinaryTree226.MainInvertBinaryTree();
-        // BinaryTree.MainBinaryTree2();
-        // Graph.MainGraph();
-        // OrderExercise.MainOrderExercise();
-        // BinaryTreeInorder94.MainBinaryTreeInorder();
-        // MaximumDepthOfBinaryTree104.MainMaximumDepthOfBinaryTree();
-        // PathSum112.MainPathSum();
-        // BinaryTreeLevelOrderTraversal102.MainBinaryTreeLevelOrderTraversal();
-        // CloneGraph133.MainCloneGraph();
-        //[[7,7,5],[2,4,6],[8,2,0]]
-        // LongestIncreasingPathInAMatrix329.MainLongestIncreasingPath();
-    }    
+        RangeSumQuery303.MainRangeSum();
+        BinarySearch704.MainBinarySearch();
+        BinaryTree.MainBinaryTree();
+        InvertBinaryTree226.MainInvertBinaryTree();
+        BinaryTree.MainBinaryTree2();
+        Graph.MainGraph();
+        BinaryTreeInorder94.MainBinaryTreeInorder();
+        MaximumDepthOfBinaryTree104.MainMaximumDepthOfBinaryTree();
+        PathSum112.MainPathSum();
+        BinaryTreeLevelOrderTraversal102.MainBinaryTreeLevelOrderTraversal();
+        CloneGraph133.MainCloneGraph();
+        // [[7,7,5],[2,4,6],[8,2,0]]
+        LongestIncreasingPathInAMatrix329.MainLongestIncreasingPath();
+    }
 }
